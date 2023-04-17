d48961890ed3532ec9a9cf2cc13402e7f7122cc8
SetMedinaVZ <74511637+SetMedinaVZ@users.noreply.github.com>
2023-04-17 12:18:33 -0600

    React Migration & Firebase Setup

---
+++ heb_ar/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCn2-JqF64DbFbjPkqNU8_jcDx2FrJLkfM",
+    authDomain: "hebar-e5.firebaseapp.com",
+    projectId: "hebar-e5",
+    storageBucket: "hebar-e5.appspot.com",
+    messagingSenderId: "951134076116",
+    appId: "1:951134076116:web:e069b8c6b7766adfbe0f41"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
