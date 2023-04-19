d4ef639044ec97be18a790790dd737c02bf824cb
SetMedinaVZ <sebastian.medina252@gmail.com>
2023-04-19 08:47:13 -0600

    Authentication Provider

--- heb_ar/firebase.js
+++ heb_ar/firebase.js
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export constants
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
---
+++ heb_ar/src/Context/AuthContext.js
@@ -0,0 +1,56 @@
+import {createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut} from "firebase/auth";
+import {createContext, useContext, useEffect, useState} from "react";
+import {auth, firestore} from "../firebase";
+import {collection, doc, getDoc} from "firebase/firestore";
+
+const AuthContext = createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({children}) {
+    const [currentUser, setCurrentUser] = useState();
+    const [userDoc, setUserDoc] = useState();
+    const [loading, setLoading] = useState(true);
+
+    function signup(email, password) {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    function login(email, password) {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    function logout() {
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        return onAuthStateChanged(auth, async user => {
+            setCurrentUser(user)
+            if(user) {
+                const userRef = doc(firestore, "users", user.uid)
+                const userSnap = await getDoc(userRef)
+                let userData = userSnap.data()
+                if(userData) {
+                    setUserDoc(userData)
+                }
+            }
+        })
+    } , [])
+
+    const value = {
+        currentUser,
+        userDoc,
+        signup,
+        login,
+        logout,
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}
