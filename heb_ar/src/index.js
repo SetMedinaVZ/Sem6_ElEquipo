d24f135f2a8e5343954fa3de8c5e3e0bb241ecd3
SetMedinaVZ <sebastian.medina252@gmail.com>
2023-04-19 08:29:36 -0600

    Tailwind & StyledComponents & Router

--- heb_ar/src/index.js
+++ heb_ar/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+    BrowserRouter,
+    Route,
+} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Route path="/" element={<App />} />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
