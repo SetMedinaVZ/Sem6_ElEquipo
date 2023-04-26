6a30ee6faa6a4740ee1d4ad5559f9d4f0fcf26ba
josegerardo9 <cantujosegerardo@clycsa.com>
2023-04-26 16:08:45 -0600

    Alistar Quests

--- heb_ar/src/pages/Quests/Quests.jsx
+++ heb_ar/src/pages/Quests/Quests.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import AppBar from "../../common/AppBar/AppBar";
 import NavBar from "../../common/NavBar/NavBar";
+import { Titulo } from "./QuestsStyled"
 
 function Quests() {
   return (
     <>
       <AppBar />
       <div className="container">
-        <h1>Quests</h1>
+        <Titulo>Tus Quests</Titulo>
       </div>
       <NavBar pagina={'quests'}/>
     </>
