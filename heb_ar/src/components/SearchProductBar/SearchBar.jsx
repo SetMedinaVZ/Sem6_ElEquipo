064bd712bde07c96d66ec7460006ffb3140a148b
MarceloM123 <58484864+MarceloM123@users.noreply.github.com>
2023-04-27 09:35:10 -0600

    Se termino el front-end del searchbar

--- heb_ar/src/components/SearchProductBar/SearchBar.jsx
+++ heb_ar/src/components/SearchProductBar/SearchBar.jsx
@@ -1,18 +1,38 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import SearchGlass from '../../assets/icons/search_glass.svg';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	position: relative;
+	margin: 20px;
+`;
 
 const Bar = styled.input`
-  background: #ffffff;
-  border: 0.3px solid #bfbfbf;
-  border-radius: 20px;
+	background: #FFFFFF;
+	border: 0.3px solid #BFBFBF;
+	border-radius: 20px;
+	height: 40px;
+	width: 90vw;
+	padding-left: 40px;
+`;
+
+const Icon = styled.img`
+	position: absolute;
+	box-sizing: border-box;
+	top: 50%;
+	left: 10px;
+	transform: translateY(-50%);
 `;
 
 const SearchBar = () => {
-  return (
-    <>
-      <Bar placeholder="Buscar productos" type="text" />
-    </>
-  );
-};
+	return (
+		<>
+			<Wrapper>
+				<Icon src={SearchGlass} alt='Magnifying glass' />
+				<Bar placeholder='Buscar productos' type='text' />
+			</Wrapper>
+		</>
+	)
+}
 
-export default SearchBar;
+export default SearchBar
--- heb_ar/src/pages/Home/home.jsx
+++ heb_ar/src/pages/Home/home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import AppBar from "../../common/AppBar/AppBar";
 import NavBar from "../../common/NavBar/NavBar";
 import CategoryList from "../../components/CategoryList/CategoryList";
+import SearchBar from "../../components/SearchProductBar/SearchBar";
 
 const Home = () => {
   return (
     <>
       <AppBar />
       <div className="container">
+				<SearchBar />
         <CategoryList />
       </div>
       <NavBar pagina={"home"} />
