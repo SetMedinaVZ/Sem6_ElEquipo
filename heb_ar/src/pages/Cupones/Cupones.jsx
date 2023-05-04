adbe991dd87e6e5f4ba247f0303ba2bd0792168b
MarceloM123 <58484864+MarceloM123@users.noreply.github.com>
2023-05-04 10:49:43 -0600

    Posición cupón e img heb posicionado correctamente, falta agregar texto

--- heb_ar/src/pages/Cupones/Cupones.jsx
+++ heb_ar/src/pages/Cupones/Cupones.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect}  from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "../../common/AppBar/AppBar";
 import NavBar from "../../common/NavBar/NavBar";
 import { Titulo } from "./CuponesStyled";
@@ -18,22 +18,23 @@ function Cupones() {
       <div className="container">
         <Titulo>Cupones por tiempo</Titulo>
         <div className="Tiempo">
-          <img src={Info} className="InfoSvg"/>
+          <img src={Info} className="InfoSvg" />
           <h1 className="TimeVar">{minutes + ":" + seconds}</h1>
         </div>
         <div className="CuponesLayout">
-          <div className="NivelCupon">
-            <h1>Nivel 1</h1>
+          <div className="cupon">
+            <div className="NivelCupon">
+              <h1>Nivel 1</h1>
+            </div>
+            <div className="cupon-info">
+              <img src={CuponImg} alt="Cupon" className="cupon-img" />
+              <img src={HebIMG} alt="HEB" className="heb-img" />
+              {/* <p>5% de descuento en la compra de un paquete de tortillas de maiz de marcas HEB</p> */}
+            </div>
           </div>
-          {/* <div className="cupon"> */}
-          <img src={CuponImg} alt="" className="cupon-img" />
-          <div className="InfoCupon">
-            <img src={HebIMG} alt="" className="cupon-heb" />
-          </div>
-          {/* </div> */}
         </div>
       </div>
-      <NavBar pagina={'cupones'}/>
+      <NavBar pagina={'cupones'} />
     </>
   );
 }
