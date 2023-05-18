e05f3ac6e49bea116c219c55988c00d29c98d2e0
josegerardo9 <a00830760@tec.mx>
2023-05-18 09:38:43 -0600

    Maquetación GastosMensuales

--- heb_ar/src/pages/GastosMensuales/GastosMensuales.jsx
+++ heb_ar/src/pages/GastosMensuales/GastosMensuales.jsx
@@ -2,7 +2,23 @@ import React from "react";
 // import AppBar from "../../common/AppBar/AppBar";
 // import NavBar from "../../common/NavBar/NavBar";
 import Arrow from "../../assets/icons/arrow.svg"
-import {Titulo, Back} from "./GastosMensualesStyled"
+import {
+  Titulo, 
+  Back, 
+  Total, 
+  PieDiv, 
+  FyVLegendDiv,  
+  LegendDiv, 
+  FyVColorDiv,
+  PColorDiv, 
+  CyPColorDiv,
+  PyTColorDiv,
+  LColorDiv,
+  DColorDiv,
+  TextDiv, 
+  Cat, 
+  PorDesc, 
+  Spent} from "./GastosMensualesStyled"
 import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js';
 import {Pie} from 'react-chartjs-2';
 
@@ -17,18 +33,18 @@ ChartJS.register(
 function GastosMensuales() {
 
     const data = {
-        labels: ['Frutas Y Verduras', 'Carnes Y Pescados', 'Jamones, Quesos Y Deli', 'Pan Y Tortillas', 'Lácteos Y Huevo', 'Vinos, Licores Y Cervezas', 'Alimentos Congelados', 'Bebidas Y Snacks'],
+        // labels: ['Frutas Y Verduras', 'Panadería', 'Carnes Y Pescados', 'Licores Y Cervezas', 'Lácteos', 'Dulcería'],
         datasets: [
             {
-                data: [5, 5, 5, 5, 5, 5, 5, 5],
-                backgroundColor: ['#F85A46', '#CAFFB1', '#FFB1B1', '#FFC0B1', '#FFD7B1', '#FFFCB1', '#F85A16', '#CAFFE1']
+                data: [540, 270, 202.5, 135, 135, 67.5], 
+                backgroundColor: ['#F85A46', '#FFFCB1', '#FFD7B1', '#FFB1B1', '#CAFFB1', '#FFC0B1']
             }
-        ]
+        ],
     };
 
     const options = {
 
-    }
+    };
 
     return (
       <>
@@ -38,11 +54,67 @@ function GastosMensuales() {
             <Back src={Arrow} alt="Regresar"/>
           </Link>
           <Titulo>Gastos Mensuales</Titulo>
+          <Total>$1350</Total>
+          <PieDiv>
+            <Pie
+              data = {data}
+              options = {options}
+            />
+          </PieDiv>
+          
+          <FyVLegendDiv>
+            <FyVColorDiv></FyVColorDiv>
+            <TextDiv>
+              <Cat>Frutas y verduras</Cat>
+              <PorDesc>40% de tu total del mes</PorDesc>
+            </TextDiv>
+            <Spent>$540</Spent>
+          </FyVLegendDiv>
 
-          <Pie
-            data = {data}
-            options = {options}
-          />
+          <LegendDiv>
+            <PColorDiv></PColorDiv>
+            <TextDiv>
+              <Cat>Panadería</Cat>
+              <PorDesc>20% de tu total del mes</PorDesc>
+            </TextDiv>
+            <Spent>$270</Spent>
+          </LegendDiv>
+
+          <LegendDiv>
+            <CyPColorDiv></CyPColorDiv>
+            <TextDiv>
+              <Cat>Carnes y pescados</Cat>
+              <PorDesc>15% de tu total del mes</PorDesc>
+            </TextDiv>
+            <Spent>$202.5</Spent>
+          </LegendDiv>
+
+          <LegendDiv>
+            <PyTColorDiv></PyTColorDiv>
+            <TextDiv>
+              <Cat>Licores Y Cervezas</Cat>
+              <PorDesc>10% de tu total del mes</PorDesc>
+            </TextDiv>
+            <Spent>$135</Spent>
+          </LegendDiv>
+
+          <LegendDiv>
+            <LColorDiv></LColorDiv>
+            <TextDiv>
+              <Cat>Lácteos</Cat>
+              <PorDesc>10% de tu total del mes</PorDesc>
+            </TextDiv>
+            <Spent>$135</Spent>
+          </LegendDiv>
+
+          <LegendDiv>
+            <DColorDiv></DColorDiv>
+            <TextDiv>
+              <Cat>Dulcería</Cat>
+              <PorDesc>5% de tu total del mes</PorDesc>
+            </TextDiv>
+            <Spent>$67.5</Spent>
+          </LegendDiv>
           
           
         </div>
--- heb_ar/src/pages/GastosMensuales/GastosMensualesStyled.js
+++ heb_ar/src/pages/GastosMensuales/GastosMensualesStyled.js
@@ -21,3 +21,121 @@ export const Back = styled.img`
     top: 23px;
     transform: scaleX(-1);
 `;
+
+export const Total = styled.text`
+    position: relative;
+    width: 350px;
+    height: 40px;
+    text-align: center;
+    /* left: 20px; */
+    top: 21px;
+
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 54px;
+    bottom: 11px;
+`;
+
+export const PieDiv = styled.div`
+    position: relative;
+    top: 21px;
+`;
+
+export const FyVLegendDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 65px;
+    
+    justify-content: space-between;
+    align-items: center;
+    width: 300px;
+    height: 36px;
+`;
+
+export const FyVColorDiv = styled.div`
+    height: 25px;
+    width: 25px;
+    background-color: #F85A46;
+    margin-right: 10px;
+`;
+
+export const LegendDiv = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+    
+    justify-content: space-between;
+    align-items: center;
+    width: 300px;
+    height: 36px;
+`;
+
+export const PColorDiv = styled.div`
+    height: 25px;
+    width: 25px;
+    background-color: #FFFCB1;
+    margin-right: 10px;
+`;
+
+export const CyPColorDiv = styled.div`
+    height: 25px;
+    width: 25px;
+    background-color: #FFD7B1;
+    margin-right: 10px;
+`;
+
+export const PyTColorDiv = styled.div`
+    height: 25px;
+    width: 25px;
+    background-color: #FFB1B1;
+    margin-right: 10px;
+`;
+
+export const LColorDiv = styled.div`
+    height: 25px;
+    width: 25px;
+    background-color: #CAFFB1;
+    margin-right: 10px;
+`;
+
+export const DColorDiv = styled.div`
+    height: 25px;
+    width: 25px;
+    background-color: #FFC0B1;
+    margin-right: 10px;
+`;
+
+export const TextDiv = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    margin-right: auto;
+`;
+
+export const Cat = styled.text`
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 19px;
+`;
+
+export const PorDesc = styled.text`
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 15px;
+`;
+
+export const Spent = styled.text`
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 36px;
+`;
+
