e2113102d52d1a2ef58e83cc4854ad91eac87a76
josegerardo9 <a00830760@tec.mx>
2023-05-18 17:54:48 -0600

    ScavengerHunt Update

--- heb_ar/src/pages/ScavengerHunt/ScavengerHunt.jsx
+++ heb_ar/src/pages/ScavengerHunt/ScavengerHunt.jsx
@@ -2,7 +2,7 @@ import React from "react";
 // import AppBar from "../../common/AppBar/AppBar";
 // import NavBar from "../../common/NavBar/NavBar";
 import Arrow from "../../assets/icons/arrow.svg"
-import {Titulo, Back} from "./ScavengerHuntStyled"
+import {Titulo, Back, Progress, ProgressTxt, Txt} from "./ScavengerHuntStyled"
 import { Link, useNavigate} from "react-router-dom";
 
 function GastosMensuales() {
@@ -18,6 +18,10 @@ function GastosMensuales() {
             <Back src={Arrow} alt="Regresar"/>
           </div>
           <Titulo>Scavenger Hunt</Titulo>
+          <Progress>
+            <ProgressTxt>1/5 completados</ProgressTxt>
+          </Progress>
+          <Txt>En una zona de la sucursal se encuentran 5 objetos escondidos, es tu misión encontrarlos utilizando la camara de tu celular con realidad aumentada.</Txt>
           {/* </Link> */}
         </div>
     </>
--- heb_ar/src/pages/ScavengerHunt/ScavengerHuntStyled.js
+++ heb_ar/src/pages/ScavengerHunt/ScavengerHuntStyled.js
@@ -21,3 +21,40 @@ export const Back = styled.img`
     top: 23px;
     transform: scaleX(-1);
 `;
+
+export const Progress = styled.div` 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    width: 350px;
+    height: 70px;
+    left: 20px;
+    margin-top: 21px;
+
+    background: #B3E8EF;
+    border-radius: 20px;
+`;
+
+export const ProgressTxt = styled.text` 
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 39px;
+
+    color: #3F3838;
+`;
+
+export const Txt = styled.text` 
+    display: flex;
+    width: 350px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 19px;
+
+
+    color: #787878;
+`;
