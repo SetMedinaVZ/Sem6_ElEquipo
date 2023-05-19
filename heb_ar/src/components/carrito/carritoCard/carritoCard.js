fdfbc7379ab698fd3f0fc8b68601a85c6353858e
Fausto Pinto Cabrera <83981597+Fausto1712@users.noreply.github.com>
2023-05-19 12:09:32 -0600

    Carrito Final

--- heb_ar/src/components/carrito/carritoCard/carritoCard.js
+++ heb_ar/src/components/carrito/carritoCard/carritoCard.js
@@ -4,11 +4,16 @@ import { POST_CARRITO } from "../../../graphql/mutations/postCarrito";
 import { DELETE_CARRITO } from "../../../graphql/mutations/deleteCarrito";
 import "./carritoCard.css";
 
-function CarritoCard({name,priceU,amountI,size,uid}) {
+function CarritoCard({name,priceU,amountI,size,uid, onChange}) {
     const [deleteCarrito] = useMutation(DELETE_CARRITO);
     const [postCarrito] = useMutation(POST_CARRITO);
+    const [isVisible, setIsVisible] = useState(true);
     var [priceF, setPriceF] = useState("");
     var [amount, setAmount] = useState(amountI);
+
+    const handleClick = () => {
+      setIsVisible(false);
+    };
 
     const handleUpdateCarrito = async (cantidad) => {
         try {
@@ -30,21 +35,23 @@ function CarritoCard({name,priceU,amountI,size,uid}) {
             uid: uid,
           },
         });
-
-        console.log('Deleted carrito:', data.uid);
+        handleClick();
+        onChange(priceU * amount * -1);
       } catch (error) {
         console.error('Error deleting user:', error);
       }
     };
     
+
     useEffect(() => {
-        setPriceF(priceU * amount);
+      setPriceF(priceU * amount);
     });
 
     const sumar = () => {
         setAmount(amount + 1);
         setPriceF(amount * priceU);
         handleUpdateCarrito(amount+1)
+        onChange(priceU)
     };
 
     const restar = () => {
@@ -52,31 +59,34 @@ function CarritoCard({name,priceU,amountI,size,uid}) {
             setAmount(amount - 1);
             setPriceF(amount);
             handleUpdateCarrito(amount-1)
+            onChange(priceU * -1)
         }
     };
 
     return (
       <>
-        <div className='carrito-card-container'>
-            <div className='img-placeholder'></div>
-            <div className='col'>
-                <div className='row'>
-                    <div className='col'>
-                        <h1 className='product'>{name}</h1>
-                        <h1 className='size'>{size}</h1>
-                    </div>
-                    <div onClick={handleDeleteCarrito} className='close'></div>
-                </div>
-                <div className='row price-row'>
-                    <h1 className='price'>${priceF}</h1>
-                    <div className='amount-container'>
-                        <button onClick={restar} className='amount-button'>-</button>
-                        <h1 className='amount'>{amount}</h1>
-                        <button onClick={sumar} className='amount-button'>+</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+       {isVisible && (
+          <div className='carrito-card-container'>
+              <div className='img-placeholder'></div>
+              <div className='col'>
+                  <div className='row'>
+                      <div className='col'>
+                          <h1 className='product'>{name}</h1>
+                          <h1 className='size'>{size}</h1>
+                      </div>
+                      <div onClick={handleDeleteCarrito} className='close'></div>
+                  </div>
+                  <div className='row price-row'>
+                      <h1 className='price'>${priceF}</h1>
+                      <div className='amount-container'>
+                          <button onClick={restar} className='amount-button'>-</button>
+                          <h1 className='amount'>{amount}</h1>
+                          <button onClick={sumar} className='amount-button'>+</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        )}
       </>
     );
   }
--- heb_ar/src/pages/Carrito/Carrito.jsx
+++ heb_ar/src/pages/Carrito/Carrito.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useEffect, useState} from "react";
 import AppBar from "../../common/AppBar/AppBar";
 import NavBar from "../../common/NavBar/NavBar";
 import CarritoCard from "../../components/carrito/carritoCard/carritoCard";
@@ -7,17 +7,27 @@ import { useQuery } from "@apollo/client";
 import { GET_CARRITO } from "../../graphql/queries/getCarrito";
 
 function Carrito() {
-  var newPriceTotal = 0
-  const { loading, error, data } = useQuery(GET_CARRITO);
+  var [PriceTotal, setPriceTotal] = useState(0);
+  var newPriceTotal = 0;
+  const { loading, error, data } = useQuery(GET_CARRITO,{fetchPolicy: 'network-only',});
   let carritoList = [];
+
+  const handleChildChange = (value) => {
+    setPriceTotal(PriceTotal + value);
+  };
+
   if (data !== undefined) {
     data.carrito.forEach(product => {
-      carritoList.push(<CarritoCard name={product.name} size={product.size} priceU={product.precioU} amountI={product.cantidad} uid={product.uid}></CarritoCard>);
-      newPriceTotal = newPriceTotal + (product.precioU * product.cantidad);
+      carritoList.push(<CarritoCard onChange={handleChildChange} name={product.name} size={product.size} priceU={product.precioU} amountI={product.cantidad} uid={product.uid}></CarritoCard>);
+      newPriceTotal = newPriceTotal + (product.cantidad * product.precioU);
     });
   }
 
-
+  useEffect(() => {
+    if (!loading && data) {
+      setPriceTotal(newPriceTotal);
+    }
+  }, [loading, data]);
 
   return (
     <>
@@ -33,7 +43,7 @@ function Carrito() {
           <Space/>
           <Price>
             <p>Total: </p>
-            <p>${newPriceTotal}</p>
+            <p>${PriceTotal}</p>
           </Price>
           <Next>Continuar al checkout</Next>
         </div>
