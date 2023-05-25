e4202b9b59c61506faaa874ca80ef8fa0093143a
josegerardo9 <a00830760@tec.mx>
2023-05-25 09:59:27 -0600

    Overview Quests V1

--- heb_ar/src/pages/Quests/Quests.jsx
+++ heb_ar/src/pages/Quests/Quests.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import AppBar from "../../common/AppBar/AppBar";
 import NavBar from "../../common/NavBar/NavBar";
-import { Titulo } from "./QuestsStyled"
+import SP from "../../assets/imgs/quest_img_4.svg"
+import SH from "../../assets/imgs/quest_img_3.svg"
+import BP from "../../assets/imgs/quest_img_2.svg"
+import QB from "../../assets/imgs/quest_img_1.svg"
+import Light from "../../assets/imgs/quest_img_5.svg"
+import { 
+  Titulo, 
+  Column, 
+  Row, 
+  ScanProducts, 
+  ScavengerHunt, 
+  BuyProducts, 
+  QuickBuy, 
+  Counter,
+  SPimg,
+  SHimg,
+  BPimg,
+  QBimg,
+  Line,
+  ConsejoDiv,
+  ConsejoTxt,
+  ConsejoImgDiv,
+  ConsejoImg,
+  ConsejoDesc
+} from "./QuestsStyled"
 
 function Quests() {
   return (
@@ -9,6 +33,51 @@ function Quests() {
       <AppBar />
       <div className="container">
         <Titulo>Tus Quests</Titulo>
+
+        {/* Progres Bar */}
+
+        <Column>
+          <Row>
+            <ScanProducts>
+              <Counter>1/4</Counter>
+              <SPimg src={SP} />
+            </ScanProducts>
+            
+            <ScavengerHunt>
+              <Counter>1/5</Counter>
+              <SHimg src={SH} />
+            </ScavengerHunt>
+          </Row>
+
+          <Row>
+            <BuyProducts>
+              <Counter>3/4</Counter>
+              <BPimg src={BP} />
+            </BuyProducts>
+            
+            <QuickBuy>
+              <Counter>5/7</Counter>
+              <QBimg src={QB} />
+            </QuickBuy>
+          </Row>
+        </Column>
+
+        <Line></Line>
+        
+        <ConsejoDiv>
+          <ConsejoTxt>Consejos para las misiones</ConsejoTxt>
+          
+          <ConsejoImgDiv>
+            
+            <ConsejoImg>
+              <img src={Light}/>
+            </ConsejoImg>
+            
+            <ConsejoDesc>Para completar las misiones de escaneo puedes usar el croquis incluido en la app</ConsejoDesc>
+          
+          </ConsejoImgDiv>
+        </ConsejoDiv>
+
       </div>
       <NavBar pagina={'quests'}/>
     </>
--- heb_ar/src/pages/Quests/QuestsStyled.js
+++ heb_ar/src/pages/Quests/QuestsStyled.js
@@ -11,3 +11,175 @@ export const Titulo = styled.text`
 
     color: #722017;
 `;
+
+export const Column = styled.div`
+    //
+`;
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 13px;
+`;
+
+export const ScanProducts = styled.div`
+    display: flex;
+    position: relative;
+    margin-right: 11px;
+    width: 165px;
+    height: 165px;
+
+    background: #FFF48F;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 15px;
+`;
+
+export const ScavengerHunt = styled.div`
+    display: flex;
+    position: relative;
+    margin-left: 12px;
+    width: 165px;
+    height: 165px;
+
+    background: #B3E8EF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 15px;
+`;
+
+export const BuyProducts = styled.div`
+    display: flex;
+    position: relative;
+    margin-right: 11px;
+    width: 165px;
+    height: 165px;
+
+    background: #C4F29D;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 15px;
+`;
+
+export const QuickBuy = styled.div`
+    display: flex;
+    position: relative;
+    margin-left: 12px;
+    width: 165px;
+    height: 165px;
+
+    background: #FFB1B1;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 15px;
+`;
+
+export const Counter = styled.text`
+    display: flex;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    margin-top: 9px;
+    margin-left: 12px;
+
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 28px;
+    line-height: 34px;
+`;
+
+export const SPimg = styled.img`
+    width: 104px;
+    height: 125px;
+
+    z-index: 9;
+
+    margin-top: 40px;
+    margin-left: 61px;
+`;
+
+export const SHimg = styled.img`
+    width: 130px;
+    height: 137px;
+
+    z-index: 9;
+
+    margin-top: 28px;
+    margin-left: 35px;
+`;
+
+export const BPimg = styled.img`
+    width: 135px;
+    height: 128px;
+
+    z-index: 9;
+
+    margin-top: 37px;
+    margin-left: 26px;
+`;
+
+export const QBimg = styled.img`
+    width: 123px;
+    height: 126px;
+
+    z-index: 9;
+
+    margin-top: 42px;
+    margin-left: 42px;
+`;
+
+export const Line = styled.hr`
+    margin-top: 17px;
+    margin-bottom: 10px;
+    
+    width: 350px;
+    height: 0px;
+    
+    border: 1px solid #BFBFBF;
+`;
+
+export const ConsejoDiv = styled.div`
+    /* margin-left: 18px; */
+`;
+
+export const ConsejoTxt = styled.text`
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 24px;
+
+    display: flex;
+    margin-bottom: 13px;
+
+    color: #787878;
+`;
+
+export const ConsejoImgDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+export const ConsejoImg = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 14px;
+    
+    width: 165px;
+    height: 165px;
+
+    background: #E8D8EC;
+    border-radius: 15px;
+`;
+
+export const ConsejoDesc = styled.text`
+    width: 174px;
+    height: 118px;
+    align-self: center;
+
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 22px;
+
+    color: #3F3838;
+`;
